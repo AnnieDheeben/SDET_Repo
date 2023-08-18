5c14ea272f5f2611b77a03a75b31b78904cdb61f
annie <annie@Annie-PC>
2023-08-18 22:48:41 +0530

    AddDigits_commit

--- src/main/java/mandatoryHomeWork/foundation/AddDigits.java
+++ src/main/java/mandatoryHomeWork/foundation/AddDigits.java
@@ -8,8 +8,8 @@ public class AddDigits {
 	/*
 	 *  * Given an integer num, repeatedly add all its digits until the result 
 	 * has only one digit, and return it.
-	 */
-	 /*  Example 1:
+	 *
+	 *  Example 1:
 	  * Input: num = 38
 		*Output: 2
 	*Explanation: The process is
