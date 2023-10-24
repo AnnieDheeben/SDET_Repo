e247b1aff9c617ebb9f4d1f5c699d19c09b9637e
ADMIN <ADMIN@DESKTOP-3FKAN2S>
2023-10-24 21:19:41 +0530

    commit

---
+++ src/main/java/DSA/CW/RearrangeWords.java
@@ -0,0 +1,20 @@
+package DSA.CW;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class RearrangeWords {
+@Test
+public void test1() {
+	String input = "Leetcode is cool";
+	String expOutput = "Is cool leetcode";
+	String actOutput = arrangeWords(input);
+	Assert.assertEquals(expOutput, actOutput);
+}
+
+private String arrangeWords(String input) {
+	// TODO Auto-generated method stub
+	return null;
+}
+}
